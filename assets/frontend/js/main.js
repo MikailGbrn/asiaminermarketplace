35d9af2d3140dba62a6d6357e902f88efb26b302
MikailGbrn <gibranlubis505@gmail.com>
2020-10-01 23:10:09 +0700

    update filter section

--- assets/frontend/js/main.js
+++ assets/frontend/js/main.js
@@ -11,10 +11,10 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+    if (panel.style.display === "none") {
+      panel.style.display = "block";
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.display = "none";
     }
   });
 }
